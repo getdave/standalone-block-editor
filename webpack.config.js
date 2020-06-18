3ef5c7cbee3b2e9dc917980b963a55e302603829
Dave Smith <getdavemail@gmail.com>
2020-06-18 10:03:52 +0100

    Remove Webpack custom CSS config - now part of wp-scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require( 'path' );
 
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
 const rootDir = path.resolve( __dirname );
@@ -12,42 +11,6 @@ const paths = {
 
 module.exports = {
 	...defaultConfig,
-	module: {
-		...defaultConfig.module,
-		rules: [
-			...defaultConfig.module.rules,
-			{
-				test: /\.(sa|sc|c)ss$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							publicPath: path.resolve( __dirname, 'build' ),
-						},
-					},
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							ident: 'postcss',
-							plugins: [ require( 'autoprefixer' )() ],
-							// sourceMap: isDevelopment
-						},
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							//sourceMap: isDevelopment
-						},
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		...defaultConfig.plugins,
-		new MiniCssExtractPlugin(),
-	],
 	resolve: {
 		...defaultConfig.resolve,
 		// alias directories to paths you can use in import() statements
